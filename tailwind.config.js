3fadfe99328e26da7096719f6e7ee0594905f99c
Muskaan <muskaangupta98@gmail.com>
2020-08-12 19:09:12 +0530

    final commit

---
+++ tailwind.config.js
@@ -0,0 +1,24 @@
+module.exports = {
+  purge: [],
+  theme: {
+    extend: {
+      fontFamily: {
+        nice: ["Pacifico"],
+      },
+      fontSize: {
+        xs: "0.75rem",
+        sm: "0.875rem",
+        base: "1rem",
+        lg: "1.125rem",
+        xl: "2.25rem",
+        "2xl": "1.5rem",
+        "3xl": "1.875rem",
+        "4xl": "2.25rem",
+        "5xl": "3rem",
+        "6xl": "6rem",
+      },
+    },
+  },
+  variants: {},
+  plugins: [],
+};
